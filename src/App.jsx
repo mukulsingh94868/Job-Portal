5b919284beee3c91a7071ec80b5ff877a3258305
@Mukul <mukul.singh@zentekinfosoft.com>
2024-09-12 17:58:27 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
       Hello World
+
+      <Button>Hello World</Button>
     </>
   )
 }
