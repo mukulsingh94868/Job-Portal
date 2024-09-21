0b0f0c789e532c5008d2cf6ce86e4bfa09edfc18
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-09-21 18:13:51 +0530

    add clerk and supabase env files

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignInButton,
+} from "@clerk/clerk-react";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <nav className="py-4 flex justify-between items-center">
+      <Link to="/">
+        <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
+      </Link>
 
-export default Header
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <div className="flex gap-8">
+        {/* <SignedOut>
+            <Button variant="outline" onClick={() => setShowSignIn(true)}>
+              Login
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            {user?.unsafeMetadata?.role === "recruiter" && (
+              <Link to="/post-job">
+                <Button variant="destructive" className="rounded-full">
+                  <PenBox size={20} className="mr-2" />
+                  Post a Job
+                </Button>
+              </Link>
+            )}
+          </SignedIn> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/layout/app-layout.jsx
+++ src/layout/app-layout.jsx
@@ -11,7 +11,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by Mukul Singh
+        Made with Mukul Singh
       </div>
     </div>
   );
