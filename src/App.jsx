8f61e9a34284e7c6ecf5e5c9c8ffe3ab792d2dc6
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-09-21 17:29:57 +0530

    intitalize routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layout/app-layout";
+import LandingPage from "./pages/landing";
+import OnBoarding from "./pages/onBoarding";
+import JobListing from "./pages/jobListing";
+import PostJob from "./pages/post-job";
+import MyJobs from "./pages/my-jobs";
+import Savedjobs from "./pages/saved-jobs";
+import JobPage from "./pages/job";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <OnBoarding />,
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+      {
+        path: "/saved-jobs",
+        element: <Savedjobs />,
+      },
+      {
+        path: "/job/:id",
+        element: <JobPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-      Hello World
-
-      <Button>Hello World</Button>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/saved-jobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Savedjobs = () => {
+  return (
+    <div>
+      Savedjobs
+    </div>
+  )
+}
+
+export default Savedjobs
